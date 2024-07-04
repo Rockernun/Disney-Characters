c4ce71fdd839b16263b2ea84d94156a714fc6bb4
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-05 00:17:02 +0900

    Updates

--- src/theme.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultTheme } from "styled-components";
-
-export const darkTheme: DefaultTheme = {
-    bgColor:"#FF98FE",
-    textColor:"white",
-    accentColor:"#9400D3",
-    transformColor:"#FF1493"
-};
-
