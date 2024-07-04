301f5538f26593b802c9076239bd7a51ae2a7a81
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-04 12:10:24 +0900

    Reset CSS
    스타일 초기화

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { darkTheme } from "./theme";
 import { ThemeProvider } from 'styled-components';
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+      <HelmetProvider>
       <ThemeProvider theme={darkTheme}>
         <App />
       </ThemeProvider>
-  </React.StrictMode>
+    </HelmetProvider>
+    </React.StrictMode>
 );
 
 
