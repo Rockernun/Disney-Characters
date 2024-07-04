9125ed15462a31cf42ca77a48c87e8e160d2fd38
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-04 11:46:24 +0900

    Theme
    화이트/다크 모드 테마 설정

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+    bgColor:"white",
+    textColor:"black",
+    accentColor:"tomato",
+};
+
+export const darkTheme: DefaultTheme = {
+    bgColor:"black",
+    textColor:"white",
+    accentColor:"teal",
+};
+
